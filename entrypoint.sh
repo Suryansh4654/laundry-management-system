225a429aa2e4f8b1329313b4fffa69a95fd8b18a
Suryansh Pandey <su12345pandey@gmail.com>
2026-04-30 01:44:55 +0530

    Add bootstrap admin creation for Render free plan

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,25 @@ until python manage.py migrate --noinput; do
   sleep 3
 done
 
+# Create or update a bootstrap admin when env vars are provided.
+if [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+  python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+email = '${DJANGO_SUPERUSER_EMAIL}'
+password = '${DJANGO_SUPERUSER_PASSWORD}'
+username = '${DJANGO_SUPERUSER_USERNAME:-admin}'
+user, _ = User.objects.get_or_create(email=email, defaults={'username': username})
+user.username = username
+user.role = 'ADMIN'
+user.is_staff = True
+user.is_superuser = True
+user.set_password(password)
+user.save()
+print('Bootstrap admin is ready:', email)
+"
+fi
+
 python manage.py collectstatic --noinput
 
 exec "$@"
